5a0f6848237247dbce1462b5f3f39e36cface7db
Marco Solina <Marco.Solina@ixico.com>
2021-05-19 08:37:07 +0200

    Auth with google

--- Chapter03/03_01_authentication_authorisation/AuthWithGoogle/App_Start/Startup.Auth.cs
+++ Chapter03/03_01_authentication_authorisation/AuthWithGoogle/App_Start/Startup.Auth.cs
@@ -58,11 +58,11 @@ namespace AuthWithGoogle
             //   appId: "",
             //   appSecret: "");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "blablabla",
+                ClientSecret = "blablabla"
+            });
         }
     }
 }
--- Chapter03/03_01_authentication_authorisation/READEME.md
+++ Chapter03/03_01_authentication_authorisation/READEME.md
@@ -1,2 +1,3 @@
 # Implement user authentication and authorization
 
+- Gli steps del libro non sono più validi. Google ha cambiato le schermate per registare una app. Sono riuscito comunque a registrare una app ed usare l'esempio del libro
