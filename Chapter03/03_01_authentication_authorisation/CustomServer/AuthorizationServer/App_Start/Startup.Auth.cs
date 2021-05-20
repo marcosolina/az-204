2873ce1f30a75383e356d29ab4d91017ae72306e
Marco Solina <Marco.Solina@ixico.com>
2021-05-20 08:52:07 +0200

    continuazione

--- Chapter03/03_01_authentication_authorisation/CustomServer/AuthorizationServer/App_Start/Startup.Auth.cs
+++ Chapter03/03_01_authentication_authorisation/CustomServer/AuthorizationServer/App_Start/Startup.Auth.cs
@@ -19,6 +19,8 @@ namespace AuthorizationServer
 {
     public partial class Startup
     {
+        private readonly ConcurrentDictionary<string, string> _authenticationCodes = new ConcurrentDictionary<string, string>(StringComparer.Ordinal);
+
         // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
@@ -108,6 +110,85 @@ namespace AuthorizationServer
             //    ClientId = "",
             //    ClientSecret = ""
             //});
+        }
+
+        private Task ValidateClientRedirectUri(OAuthValidateClientRedirectUriContext context)
+        {
+            if (context.ClientId == Clients.Client1.Id)
+            {
+                context.Validated(Clients.Client1.RedirectUrl);
+            }
+            else if (context.ClientId == Clients.Client2.Id)
+            {
+                context.Validated(Clients.Client2.RedirectUrl);
+            }
+            return Task.FromResult(0);
+        }
+
+        private Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            string clientId;
+            string clientSecret;
+            if (context.TryGetBasicCredentials(out clientId, out clientSecret) ||
+                context.TryGetFormCredentials(out clientId, out clientSecret))
+            {
+                if (clientId == Clients.Client1.Id &&
+                    clientSecret == Clients.Client1.Secret)
+                {
+                    context.Validated();
+                }
+                else if (clientId == Clients.Client2.Id && clientSecret == Clients.Client2.Secret)
+                {
+                    context.Validated();
+                }
+            }
+            return Task.FromResult(0);
+        }
+
+        private Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            ClaimsIdentity identity = new ClaimsIdentity(
+                new GenericIdentity(context.UserName,
+                                    OAuthDefaults.AuthenticationType),
+                                    context.Scope.Select(x => new Claim("urn:oauth:scope", x)));
+            context.Validated(identity);
+            return Task.FromResult(0);
+        }
+
+        private Task GrantClientCredentials(OAuthGrantClientCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(
+                new GenericIdentity(
+                    context.ClientId,
+                    OAuthDefaults.AuthenticationType),
+                context.Scope.Select(x => new Claim("urn:oauth:scope", x)));
+            context.Validated(identity);
+            return Task.FromResult(0);
+        }
+
+        private void CreateAuthenticationCode(AuthenticationTokenCreateContext context)
+        {
+            context.SetToken(Guid.NewGuid().ToString("n") + Guid.NewGuid().ToString("n"));
+            _authenticationCodes[context.Token] = context.SerializeTicket();
+        }
+
+        private void ReceiveAuthenticationCode(AuthenticationTokenReceiveContext context)
+        {
+            string value;
+            if (_authenticationCodes.TryRemove(context.Token, out value))
+            {
+                context.DeserializeTicket(value);
+            }
+        }
+
+        private void CreateRefreshToken(AuthenticationTokenCreateContext context)
+        {
+            context.SetToken(context.SerializeTicket());
+        }
+
+        private void ReceiveRefreshToken(AuthenticationTokenReceiveContext context)
+        {
+            context.DeserializeTicket(context.Token);
         }
     }
 }
---
+++ Chapter03/03_01_authentication_authorisation/CustomServer/AuthorizationServer/Constants/Paths.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuthorizationServer.Constants
+{
+    public class Paths
+    {
+        public const string AuthorizationServerBaseAddress = "https://localhost:44361";
+        public const string ResourceServerBaseAddress = "https://localhost:44361";
+        public const string ImplicitGrantCallBackPath = "https://localhost:44361/Home/SignIn";
+        public const string AuthorizeCodeCallBackPath = "https://localhost:44361/Manage";
+        public const string AuthorizePath = "/OAuth/Authorize";
+        public const string TokenPath = "/OAuth/Token";
+        public const string LoginPath = "/Account/Login";
+        public const string LogoutPath = "/Account/Logout";
+        public const string MePath = "/api/Me";
+    }
+}
