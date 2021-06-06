352d45fd469ec26470a5e856b5151dd818c4e299
Marco Solina <Marco.Solina@ixico.com>
2021-06-06 10:10:30 +0200

    exercise 3.2

---
+++ Chapter03/03_02_secure_cloud_solutions/AppUsingAZAppConfig/AppSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace AppUsingAZAppConfig
+{
+    public class AppSettings
+    {
+        public string SourceSASConnectionString { get; set; }
+        public string SourceAccountName { get; set; }
+        public string SourceContainerName { get; set; }
+        public string DestinationSASConnectionString { get; set; }
+        public string DestinationAccountName { get; set; }
+        public string DestinationContainerName { get; set; }
+
+        public static AppSettings LoadAppSettings()
+        {
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.AddAzureAppConfiguration(Environment.GetEnvironmentVariable("ConnectionString"));
+            IConfigurationRoot config = builder.Build();
+            AppSettings appSettings = new AppSettings();
+            appSettings.SourceSASConnectionString = config["TestAZ204:StorageAccount:Source:ConnectionString"];
+            appSettings.SourceAccountName = config["TestAZ204:StorageAccount:Source:AccountName"];
+            appSettings.SourceContainerName = config["TestAZ204:StorageAccount:Source:ContainerName"];
+            appSettings.DestinationSASConnectionString = config["TestAZ204:StorageAccount:Destination:ConnectionString"];
+            appSettings.DestinationAccountName = config["TestAZ204:StorageAccount:Destination:AccountName"];
+            appSettings.DestinationContainerName = config["TestAZ204:StorageAccount:Destination:ContainerName"];
+            return appSettings;
+        }
+    }
+}
---
+++ Chapter03/03_02_secure_cloud_solutions/AppUsingAZAppConfig/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using System;
+using Azure.Storage.Blobs;
+
+namespace AppUsingAZAppConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Copy items between Containers Demo!");
+            Task.Run(async () => await StartContainersDemo()).Wait();
+        }
+
+        public static async Task StartContainersDemo()
+        {
+            string sourceBlobFileName = "Testing.zip";
+            AppSettings appSettings = AppSettings.LoadAppSettings();
+
+            //Get a cloud client for the source Storage Account
+            BlobServiceClient sourceClient = Common.CreateBlobClientStorageFromSAS(appSettings.SourceSASConnectionString);
+            BlobServiceClient destinationClient = Common.CreateBlobClientStorageFromSAS(appSettings.DestinationSASConnectionString);
+
+            //Get a reference for each container
+            BlobContainerClient sourceContainerReference = sourceClient.GetBlobContainerClient(appSettings.SourceContainerName);
+            BlobContainerClient destinationContainerReference = destinationClient.GetBlobContainerClient(appSettings.DestinationContainerName);
+
+            //Get a reference for the source blob
+            BlobClient sourceBlobReference = sourceContainerReference.GetBlobClient(sourceBlobFileName);
+            BlobClient destinationBlobReference = destinationContainerReference.GetBlobClient(sourceBlobFileName);
+
+            //Move the blob from the source container to the destination container
+            await destinationBlobReference.StartCopyFromUriAsync(sourceBlobReference.Uri);
+        }
+    }
+}
