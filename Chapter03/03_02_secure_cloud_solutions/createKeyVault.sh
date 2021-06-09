45ec6393f1d393def16f0330e10f7d0976ce3621
Marco Solina <Marco.Solina@ixico.com>
2021-06-09 09:01:40 +0200

    script to generate the key vault

---
+++ Chapter03/03_02_secure_cloud_solutions/createKeyVault.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#Azure CLI template deployment
+
+RESOURCE_GROUP=AZ204-Marco-keyvault-rg
+RESOURCE_GROUP_LOCATION="UK South"
+DEPLOYMENT_NAME=AZ204-Marco-Dep
+ARM_TEMPLATE=arm-keyvault.json
+OVERRIDE_PROPS=keyvault-params.json
+
+# Props to override
+KEYVAULT_NAME=az204marcokv
+
+../../Misc/Scripts/selectSubscription.sh
+
+while ! [[ "$EXEC_OPTION" =~ ^(0|1)$ ]] 
+do
+  echo ""
+  echo "0) Deploy Key Vault"
+  echo "1) Delete Key Vault"
+  echo ""
+  read -p "Choose one option: " EXEC_OPTION
+  echo ""
+done
+ 
+echo "You choose $EXEC_OPTION"
+
+if [ $EXEC_OPTION = 0 ]
+then  
+
+	az webapp list --output table
+    read -p "Type the Webapp name: " WEB_APP_NAME
+	read -p "Type the WebApp resource group: " WEB_APP_RG
+
+	WEBAPP_PRINCIPAL_ID=$(az webapp identity show --name $WEB_APP_NAME --resource-group $WEB_APP_RG --query principalId --output tsv)
+
+
+	######################################
+	# Createing the resource group
+	######################################
+	az group create --name $RESOURCE_GROUP --location "$RESOURCE_GROUP_LOCATION"
+	
+	######################################
+	# I need to retrieve the Azure val
+	# for the resource group "UK South" -> "uksouth"
+	# and update the parameters that
+	# I want to use when creating the 
+	# resource using the ARM template
+	######################################
+	REGION_AZ_VAL=$(az group show --name $RESOURCE_GROUP --query location --output tsv)
+
+	NEW_OVERRIDE_PROPS=tempParam.json
+	cp $OVERRIDE_PROPS $NEW_OVERRIDE_PROPS
+	sed -i 's/KEYVAULT_NAME/'"$KEYVAULT_NAME"'/' $NEW_OVERRIDE_PROPS
+	sed -i 's/LOCATION/'"$REGION_AZ_VAL"'/' $NEW_OVERRIDE_PROPS
+	sed -i 's/WEBAPP_PRINCIPAL_ID/'"$WEBAPP_PRINCIPAL_ID"'/' $NEW_OVERRIDE_PROPS
+	
+	######################################
+	# Createing the resource in the
+	# resource group
+	######################################
+	az deployment group create \
+	--name $DEPLOYMENT_NAME \
+	--resource-group $RESOURCE_GROUP \
+	--template-file $ARM_TEMPLATE \
+	--parameters @$NEW_OVERRIDE_PROPS
+	# Note the use of the @ symbol in front of the parameters file. This @ symbol is required by the az group
+	# deployment create command.
+	
+	rm $NEW_OVERRIDE_PROPS
+	
+fi
+
+if [ $EXEC_OPTION = 1 ]
+then
+	az group delete --name $RESOURCE_GROUP --yes
+fi
+
+echo ""
+echo "End of the script"
+echo ""
